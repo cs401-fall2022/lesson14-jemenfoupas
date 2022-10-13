e0a39e5b3d167627f23e04c3c2528e9a064e58f6
Rich Boundji <rich@Richs-MacBook-Pro.local>
2022-10-12 22:06:15 -0600

    something

---
+++ Dest/script.js
@@ -0,0 +1,16 @@
+"use strict";
+let word = document.getElementById("word");
+if (word != null) {
+    word.addEventListener("mouseover", intro);
+    word.style.opacity = "0";
+}
+let timer;
+function intro() {
+    if (word != null) {
+        timer = setInterval(fade, 10);
+    }
+}
+function fade() {
+    if (word != null)
+        word.style.opacity = ".5";
+}
