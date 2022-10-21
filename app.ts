42c46a9fe33184adcef44a01ee7716ab79963aa8
rich boundji <boundjirichrich@gmail.com>
2022-10-21 11:20:08 -0600

    need to redo the information

--- app.ts
+++ app.ts
@@ -13,12 +13,25 @@ function hello (): String {
  * turns demo red
  */
  function greeting() {
-    if(document.body.style.backgroundColor != "white"){
-        document.body.style.backgroundColor = "white";
-    } else {
-        document.body.style.backgroundColor = "black";
-    }
+    // if(document.body.style.backgroundColor != "rgb(188, 195, 208)" ){
+    //     document.body.style.backgroundColor = "rgb(188, 195, 208)";
+    // } else {
+    //     document.body.style.backgroundColor = "black";
+    // }
     
+    let title = document.getElementById("firstTitle");
+    let box = document.getElementById("firstBox");
+    if(title!=null && box != null){
+        if(title.style.backgroundColor != "rgb(214, 67, 9)"){
+            title.style.backgroundColor = "rgb(214, 67, 9)";
+            title.style.borderColor = "rgb(0, 55, 161)";
+            box.style.borderColor = "rgb(0, 55, 161)"
+        } else {
+            title.style.backgroundColor = "rgb(0, 55, 161)";
+            title.style.borderColor = "rgb(214, 67, 9)";
+            box.style.borderColor = "rgb(214, 67, 9)"
+        }
+    }
       
 }
 
--- build/app.js
+++ build/app.js
@@ -10,11 +10,24 @@ function hello() {
  * turns demo red
  */
 function greeting() {
-    if (document.body.style.backgroundColor != "white") {
-        document.body.style.backgroundColor = "white";
-    }
-    else {
-        document.body.style.backgroundColor = "black";
+    // if(document.body.style.backgroundColor != "rgb(188, 195, 208)" ){
+    //     document.body.style.backgroundColor = "rgb(188, 195, 208)";
+    // } else {
+    //     document.body.style.backgroundColor = "black";
+    // }
+    var title = document.getElementById("firstTitle");
+    var box = document.getElementById("firstBox");
+    if (title != null && box != null) {
+        if (title.style.backgroundColor != "rgb(214, 67, 9)") {
+            title.style.backgroundColor = "rgb(214, 67, 9)";
+            title.style.borderColor = "rgb(0, 55, 161)";
+            box.style.borderColor = "rgb(0, 55, 161)";
+        }
+        else {
+            title.style.backgroundColor = "rgb(0, 55, 161)";
+            title.style.borderColor = "rgb(214, 67, 9)";
+            box.style.borderColor = "rgb(214, 67, 9)";
+        }
     }
 }
 export { hello, greeting };
